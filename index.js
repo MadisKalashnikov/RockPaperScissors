aeba5e824fce5b430ace2c12350c674ec77a4470
Madis <kalashnikov.madis@gmail.com>
2022-08-13 18:17:48 +0300

    Add game logic to rock paper scissors for one game Add array for computer choice Add computer and user choice

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 // choices array
-let choice = ["Rock", "Paper", "Scissors"]
+let choice = ["Rock", "Paper", "Scissors"];
 // computer choice
 let getComputerChoice = (arr) => {
-	let randomNumber = Math.floor(Math.random() * arr.length)
-	return arr[randomNumber]
+	let randomNumber = Math.floor(Math.random() * arr.length);
+	return arr[randomNumber].toLowerCase();
 }
-console.log(getComputerChoice(choice))
+let playRockPaperScissors = () => {
+	let playerSelection = prompt("choose your weapon", "rock").toLowerCase();
+	let computerSelection = getComputerChoice(choice)
+	console.log(playerSelection, computerSelection)
+	if (playerSelection === "paper" && computerSelection === "rock") {
+		return "You Win!"
+	} else if(playerSelection === "rock" && computerSelection === "scissors") {
+		return "You Win!"
+	} else if(playerSelection === "scissors" && computerSelection === "paper") {
+		return "You Win!"
+	} else if(playerSelection === computerSelection) {
+		return "It's A Draw!"
+	} else {
+		return "Computer Wins!"
+	}
+	
+	
+
+}
+console.log(playRockPaperScissors())
